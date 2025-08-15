a9c76eb6169b288f8d234b5673fa57c46e0a312b
harshita gupta <harshita.g.2k@gmail.com>
2025-08-15 22:15:38 +0530

    3 react projects added

---
+++ customReact/customreact.js
@@ -0,0 +1,10 @@
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  childern: 'click me to go to google'
+}
+
+const mainContainer = document.querySelector('#root')
