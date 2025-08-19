cc4221cde77f46991d52a4e7944c89367b0dfed0
harshita gupta <harshita.g.2k@gmail.com>
2025-08-19 17:35:04 +0530

    customReact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,37 @@ import  React  from 'react'
 import  ReactDOM  from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   childern: 'click me to go to google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const anotheruser = "Chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    { href: 'https://google.com', target: '_blank' },
+    'click me to go to google',
+    anotheruser
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    reactElement
 )
